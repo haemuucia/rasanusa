81f252c740a6163679c4d2f7ef6aae10c85e12ce
haemuucia <veronicairene315@gmail.com>
2025-07-24 23:04:37 +0700

    Initial commit

---
+++ app/Filament/Resources/TransactionResource.php
@@ -0,0 +1,146 @@
+<?php
+
+namespace App\Filament\Resources;
+
+use App\Filament\Resources\TransactionItemsResource\Pages\ListTransactionItems;
+use App\Filament\Resources\TransactionResource\Pages;
+use App\Filament\Resources\TransactionResource\RelationManagers;
+use App\Models\Transaction;
+use App\Filament\Resources\TransactionResource\Pages\ListTransaction;
+use Filament\Forms;
+use Filament\Forms\Form;
+use Filament\Resources\Resource;
+use Filament\Tables;
+use Filament\Tables\Table;
+use Illuminate\Database\Eloquent\Builder;
+use Illuminate\Database\Eloquent\SoftDeletingScope;
+use Filament\Tables\Actions\Action;
+class TransactionResource extends Resource
+{
+    protected static ?string $model = Transaction::class;
+
+    protected static ?string $navigationIcon = 'heroicon-o-rectangle-stack';
+
+    public static function form(Form $form): Form
+    {
+        return $form
+            ->schema([
+                Forms\Components\TextInput::make('code')
+                    ->required()
+                    ->maxLength(255),
+                Forms\Components\TextInput::make('name')
+                    ->required()
+                    ->maxLength(255),
+                Forms\Components\TextInput::make('phone')
+                    ->tel()
+                    ->required()
+                    ->maxLength(255),
+                Forms\Components\TextInput::make('external_id')
+                    ->required()
+                    ->maxLength(255),
+                Forms\Components\TextInput::make('checkout_link')
+                    ->required()
+                    ->maxLength(255),
+                Forms\Components\TextInput::make('barcodes_id')
+                    ->required()
+                    ->numeric(),
+                Forms\Components\TextInput::make('payment_method')
+                    ->maxLength(255)
+                    ->default(null),
+                Forms\Components\TextInput::make('payment_status')
+                    ->maxLength(255)
+                    ->default(null),
+                Forms\Components\TextInput::make('subtotal')
+                    ->required()
+                    ->numeric(),
+                Forms\Components\TextInput::make('ppn')
+                    ->required()
+                    ->numeric(),
+                Forms\Components\TextInput::make('total')
+                    ->required()
+                    ->numeric(),
+            ]);
+    }
+
+    public static function table(Table $table): Table
+    {
+        return $table
+            ->columns([
+                Tables\Columns\TextColumn::make('code')
+                    ->label('Transaction Code')
+                    ->searchable(),
+                Tables\Columns\TextColumn::make('name')
+                    ->label('Customer Name')
+                    ->searchable(),
+                Tables\Columns\TextColumn::make('phone')
+                    ->label('Phone Number')
+                    ->searchable(),
+                Tables\Columns\ImageColumn::make('barcodes.image')
+                    ->label('Barcode'),
+                Tables\Columns\TextColumn::make('payment_method')
+                    ->label('Payment Method')
+                    ->searchable(),
+                Tables\Columns\TextColumn::make('payment_status')
+                    ->label('Payment Status')
+                    ->badge()
+                    ->colors([
+                        'success' => fn ($state): bool => in_array($state, ['SUCCESS', 'PAID', 'SETTLED']),
+                        'warning' => fn ($state): bool => $state === 'PENDING',
+                        'danger' => fn ($state): bool => in_array($state, ['FAILED', 'EXPIRED']),
+                    ]),
+                Tables\Columns\TextColumn::make('external_id')
+                    ->searchable(),
+                Tables\Columns\TextColumn::make('checkout_link')
+                    ->searchable(),
+                Tables\Columns\TextColumn::make('subtotal')
+                    ->numeric()
+                    ->label('Subtotal')
+                    ->money('IDR'),
+                Tables\Columns\TextColumn::make('ppn')
+                    ->numeric()
+                    ->label('PPN')
+                    ->money('IDR'),
+                Tables\Columns\TextColumn::make('total')
+                    ->numeric()
+                    ->label('Total')
+                    ->money('IDR'),
+                Tables\Columns\TextColumn::make('created_at')
+                    ->label('Created At')
+                    ->dateTime()
+                    ->sortable()
+                    ->toggleable(isToggledHiddenByDefault: true),
+                Tables\Columns\TextColumn::make('updated_at')
+                    ->label('Updated At')
+                    ->dateTime()
+                    ->sortable()
+                    ->toggleable(isToggledHiddenByDefault: true),
+            ])
+            ->filters([])
+            ->actions([
+                Tables\Actions\EditAction::make(),
+                Action::make('See Transaction')
+                ->color('success')
+                ->url(
+                    fn (Transaction $record): string => static::getUrl('transaction-items.index', [
+                    'parent' => $record->id,
+                    ])
+                ),
+            ])
+            ->bulkActions([]);
+    }
+
+    public static function getRelations(): array
+    {
+        return [];
+    }
+
+    public static function getPages(): array
+    {
+        return [
+            'index' => Pages\ListTransactions::route('/'),
+            'create' => Pages\CreateTransaction::route('/create'),
+            'edit' => Pages\EditTransaction::route('/{record}/edit'),
+            'transaction-items.index' => ListTransactionItems::route('{pages}/listtransactionitems')
+        ];
+    }
+}
